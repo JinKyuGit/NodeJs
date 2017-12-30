9edda75953b9d9b7b654acdc2e57fffb42475724
jinkyu <wlsrb8993@gmail.com>
2017-12-31 02:36:36 +0900

    test

--- nodeWeb/routes/index.js
+++ nodeWeb/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var fs=require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//라우터 등록 방법.
+//router.get('', function(req, res){
+//	fs.readFile('', function(error, data) {
+//	res.end(data);
+//	});
+//});
 
 module.exports = router;
